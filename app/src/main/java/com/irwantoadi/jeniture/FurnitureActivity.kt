e082434332a04185b054b16930f5401147581475
adiirwanto <irwantoadi926@gmail.com>
2021-09-19 14:49:01 +0700

    project jepara integrated furniture

---
+++ app/src/main/java/com/irwantoadi/jeniture/FurnitureActivity.kt
@@ -0,0 +1,11 @@
+package com.irwantoadi.jeniture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FurnitureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_furniture)
+    }
+}
---
+++ app/src/main/java/com/irwantoadi/jeniture/KeraliyuActivity.kt
@@ -0,0 +1,11 @@
+package com.irwantoadi.jeniture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KeraliyuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keraliyu)
+    }
+}
